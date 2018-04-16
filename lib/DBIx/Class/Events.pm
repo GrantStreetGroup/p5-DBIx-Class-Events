7436247d3b238f3ca33e63e3eba924c907423927
Andrew Fresh <andrew.fresh@grantstreet.com>
2018-04-16 18:18:52 -0400

    Add boilerplate for module and test

---
+++ lib/DBIx/Class/Events.pm
@@ -0,0 +1,23 @@
+package DBIx::Class::Events;
+
+# ABSTRACT: Events for your DBIC Results
+# VERSION: 0.01
+
+use strict;
+use warnings;
+use parent 'DBIx::Class';
+
+1;
+__END__
+
+=head1 NAME
+
+DBIx::Class::Events - Events for your DBIC Results
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+None
---
+++ t/DBIx-Class-Events.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+use Test::More;
+
+use DBIx::Class::Events;
+
+pass "Nothing to see here";
+
+done_testing;
